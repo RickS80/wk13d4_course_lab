265137ada52176233034f999c064afd86aeb0c01
Rick Symington <rick.symington@gmail.com>
2019-02-28 14:37:39 +0000

    add route for finding bookings by date

--- src/main/java/com/example/bookingSystem/controllers/BookingController.java
+++ src/main/java/com/example/bookingSystem/controllers/BookingController.java
@@ -1,4 +1,25 @@
 package com.example.bookingSystem.controllers;
 
+import com.example.bookingSystem.models.Booking;
+import com.example.bookingSystem.repositories.BookingRepositories.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/bookings")
 public class BookingController {
+
+    @Autowired
+    BookingRepository bookingRepository;
+
+    @GetMapping(value = "/date/{date}")
+    public List<Booking> getBookingsByDate(@PathVariable String date){
+        return bookingRepository.getBookingsByDate(date);
+    }
+
 }
