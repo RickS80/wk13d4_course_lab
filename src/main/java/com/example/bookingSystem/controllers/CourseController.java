b915b2e45793c6af9810ae15d2a86a635f425d9a
Rick Symington <rick.symington@gmail.com>
2019-02-28 14:26:08 +0000

    add route for finding courses by rating

--- src/main/java/com/example/bookingSystem/controllers/CourseController.java
+++ src/main/java/com/example/bookingSystem/controllers/CourseController.java
@@ -1,4 +1,26 @@
 package com.example.bookingSystem.controllers;
 
+import com.example.bookingSystem.models.Course;
+import com.example.bookingSystem.repositories.CourseRepositories.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/courses")
 public class CourseController {
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @GetMapping(value = "/starRating/{starRating}")
+    public List<Course> getCoursesByStarRating(@PathVariable int starRating){
+        return courseRepository.getCoursesByStarRating(starRating);
+    }
+
+
 }
